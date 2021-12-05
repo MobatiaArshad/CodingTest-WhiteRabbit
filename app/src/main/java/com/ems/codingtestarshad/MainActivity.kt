943adaf9aa4986295ebadf961632802167341f29
Arshad <mohdard@gmail.com>
2021-12-05 10:59:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/ems/codingtestarshad/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ems.codingtestarshad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ems/codingtestarshad/fragments/allUsers/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.ems.codingtestarshad.fragments.allUsers
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
